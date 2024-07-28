d5fe8eb56d756dde40aabfb4b05c7879cf6d5ee4
xturus138 <radityaramadhan.138@gmail.com>
2024-07-28 18:49:50 +0700

    fix logout v3

--- logout.php
+++ logout.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 if(session_destroy()) {
+    // Menghapus cookie sesi
+    setcookie("login_user", "", time() - 3600, "/");
+    
+    // Mengatur headers untuk mencegah caching
+    header("Cache-Control: no-cache, no-store, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+    
+    // Redirect ke halaman login
     header("Location: index.html");
+    exit();
 }
 ?>
