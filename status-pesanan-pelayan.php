fb48473df52f4bfc9b64d6745870c28de904b9cc
RyujiKazu <rivalsetiawan200@gmail.com>
2024-07-31 00:10:25 +0700

    perbaharui status

--- status-pesanan-pelayan.php
+++ status-pesanan-pelayan.php
@@ -361,7 +361,7 @@
 
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Pengolahan Pesanan</h1>
+                <h1 class="h3 mb-0 text-gray-800">Status Pesanan</h1>
             </div>
 
             <!-- Tabel Pesanan -->
