d9b0b37d2b2107bd3b4eaf0c7f72cda1468eb448
Handa284 <raihangede28@gmail.com>
2024-08-03 19:09:14 +0700

    fix laporan keuangan kasir

--- pengolahan-laporan-keuangan-kasir(data).php
+++ pengolahan-laporan-keuangan-kasir(data).php
@@ -7,9 +7,8 @@ $end_date = isset($_GET['end_date']) ? $_GET['end_date'] : '';
 
 if ($start_date && $end_date) {
     // Query untuk mengambil data berdasarkan tanggal
-    $sql = "SELECT p.tanggal, p.total as total_penjualan, SUM(ip.jumlah) as jumlah_pesanan 
+    $sql = "SELECT p.tanggal, SUM(p.total) as total_penjualan
             FROM pesanan p
-            JOIN isi_pesanan ip ON p.no_pesanan = ip.no_pesanan
             WHERE p.tanggal BETWEEN '$start_date' AND '$end_date' 
             AND p.status_pesanan = 'sudah dibayar'
             GROUP BY p.tanggal";
@@ -17,13 +16,11 @@ if ($start_date && $end_date) {
 
     $tanggal = [];
     $total_penjualan = [];
-    $jumlah_pesanan = [];
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
             $tanggal[] = $row['tanggal'];
             $total_penjualan[] = $row['total_penjualan'];
-            $jumlah_pesanan[] = $row['jumlah_pesanan'];
         }
     } else {
         echo "Hasil tidak ditemukan";
